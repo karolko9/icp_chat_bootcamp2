ced0793da0c6b30bd133824aa1b16773ab4e5a6c
karolko9 <bolekbolek909@gmail.com>
2024-07-31 23:57:43 +0200

    mvp

--- src/icp_chat_frontend/src/App.vue
+++ src/icp_chat_frontend/src/App.vue
@@ -1,28 +1,137 @@
-<script setup>
+<script lang="ts">
 import { ref } from 'vue';
-import { icp_chat_backend } from 'declarations/icp_chat_backend/index';
-let greeting = ref('');
+import { icp_chat_backend, canisterId, createActor } from '../../declarations/icp_chat_backend';
+import { AuthClient } from '@dfinity/auth-client';
+import { HttpAgent } from '@dfinity/agent';
+import type { Identity } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
+import type { UserData } from '../../declarations/icp_chat_backend/icp_chat_backend.did';
 
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  await icp_chat_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
+export default {
+  data() {
+    return {
+      newChat: "",
+      chats: [] as string[][],
+      identity: undefined as undefined | Identity,
+      principal: undefined as undefined | Principal,
+      targetPrincipal: "",
+      userData: undefined as undefined | UserData,
+      newUsername: "",
+      allUsers: [] as [Principal, UserData][]
+    }
+  },
+  methods: {
+    isUserLogged() {
+      if (!this.identity || !this.principal || this.principal === Principal.anonymous()) {
+        throw new Error("PLZ log in")
+      }
+      return {
+        identity: this.identity,
+        principal: this.principal 
+      }
+    },
+    validateTargetPrincipal(){
+      const cleanTargetPrincipal = this.targetPrincipal.trim();
+      if (cleanTargetPrincipal === ""){
+        throw new Error("No principal")
+      }
+      const targetPrincipal = Principal.fromText(cleanTargetPrincipal)
+      if (!targetPrincipal || targetPrincipal === Principal.anonymous()){
+        throw new Error("Wrong target")
+      }
+      return targetPrincipal
+    },
+    getAuthClient(){
+      this.isUserLogged()
+      return createActor(canisterId, {
+        agentOptions: {
+          identity: this.identity
+        }
+      });
+    },
+    async dodajChatMSG() {
+      const targetPrincipal = this.validateTargetPrincipal()
+      const backend = this.getAuthClient();
+      await backend.add_chat_msg(this.newChat, targetPrincipal)
+      await this.pobierzChaty()
+    },
+    async pobierzChaty() {
+      const {identity, principal} = this.isUserLogged()
+      const targetPrincipal = this.validateTargetPrincipal()
+
+      const chatPath = [targetPrincipal, identity.getPrincipal()].sort()
+      this.chats = await icp_chat_backend.get_chat(chatPath)
+    },
+    async login() {
+      const authClient = await AuthClient.create();
+      await authClient.login({
+        identityProvider: "http://avqkn-guaaa-aaaaa-qaaea-cai.localhost:4943/",
+        onSuccess: async () => {
+          const identity = authClient.getIdentity();
+          const principal = identity.getPrincipal();
+          this.principal = principal;
+          this.identity = identity;
+          console.log("Zalogowano", this.principal)
+          await this.getUserData()
+          await this.getAllUsers()
+        }
+      })
+    },
+    async logout () {
+      const authClient = await AuthClient.create();
+      await authClient.logout()
+      this.identity = undefined;
+      this.principal = undefined;
+      this.chats = [];
+      this.userData = undefined
+    },
+    async registerUsername() {
+      const trimedUsername = this.newUsername.trim();
+      const backend = this.getAuthClient();
+      await backend.register(trimedUsername)
+      await this.getUserData()
+      await this.getAllUsers()
+    },
+    async getUserData() {
+      const {principal} = this.isUserLogged()
+      const maybeUserData = await icp_chat_backend.get_user(principal as Principal)
+      if (maybeUserData.length === 0) {
+        this.userData = undefined
+      } else {
+        this.userData = maybeUserData[0]
+      }
+      console.log("User data", this.userData)
+    },
+    async getAllUsers(){
+      this.allUsers = await icp_chat_backend.get_users()
+    }
+  },
 }
 </script>
 
 <template>
   <main>
-    <img src="/logo2.svg" alt="DFINITY logo" />
-    <br />
-    <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
+    <button v-if="!principal" @click="login">login</button>
+    <button v-if="principal" @click="logout">logout</button>
+    <div v-if="principal && !userData">
+      <input v-model="newUsername" placeholder="nick"/> <button @click="registerUsername">register</button>
+    </div>
+    <div v-if="principal && userData">
+      {{ userData.nickname }}
+      <div v-if="allUsers">
+        <select v-model="targetPrincipal">
+          <option disabled value="">Please select one</option>
+          <option v-for="[userPrincipal, userData] in allUsers" :value="userPrincipal.toText()">{{ userData.nickname }}</option>
+        </select>
+      </div>
+      <div>
+        <div v-for="chat in chats[0]">
+          {{ chat }}
+        </div>
+      </div>
+      <div>
+        <textarea v-model="newChat" placeholder="wiadomosc"></textarea><button @click="dodajChatMSG">Dodaj notatke</button>
+      </div>
+    </div>
   </main>
 </template>
